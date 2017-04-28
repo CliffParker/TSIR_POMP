24ce9e36c940fb66562b94601e1c01f2d2afe025
CliffParker <cliffparker16@live.com>
2017-04-28 07:14:19 -0500

    Fixed TSIR model
    Got it ready for the talk

--- TSIRest.R
+++ TSIRest.R
@@ -75,7 +75,7 @@ Covar = Covar[,-c(1,2)]
 Covar$birth = Fdat.d[,2]
 
 "POMP Parts"
-toEsts <- Csnippet("
+toEst <- Csnippet("
                   Tm = log(m);
                   TB1 = log(B1);
                   TB2 = log(B2);
@@ -104,11 +104,11 @@ toEsts <- Csnippet("
                   TB25 = log(B25);
                   TB26 = log(B26);
                   
-                 // Ttheta0 = log(theta0);
+                  Talpha = logit(alpha);
                   TSbar = log(Sbar);
                   ")
 
-fromEsts <- Csnippet("
+fromEst <- Csnippet("
                     Tm = exp(m);
                     TB1 = exp(B1);
                     TB2 = exp(B2);
@@ -138,114 +138,92 @@ fromEsts <- Csnippet("
                     TB26 = exp(B26);
                     
                     
-                    //Ttheta0 = exp(theta0);
+                    Talpha = expit(alpha);
                     TSbar = exp(Sbar);
                     ")
 
-
-initls <- Csnippet("
+initlz <- Csnippet("
                    I = Tcases;
                    lambda = Tcases;
-                   S =  Sbar + Z;
+                   //S =  Sbar + Z;
                    ")
 
-stochStepF <- Csnippet("
-                      double B, va;
-
-                       #define max(x, y) (((x) > (y)) ? (x) : (y))
-                       #define min(x, y) (((x) < (y)) ? (x) : (y))
-
-                     //Vacination uptake
-                       if ( t< 1968)
-                       va = 0;
-                       else if (t>=1968 && t<=1978)
-                       va = 0.4  + 0.4 * (t-1968)/10;
-                       else
-                       va = 0.8;
-                       
-                       
-                       // By-week time  seasonality
-                       int  tstar = (t - 1944)*26.01786;
-                       
+stochStep <- Csnippet("
+                      double B;
                       
-                     
-                       
-                       if (tstar  % 26 == 1)
-                       B = B1;
-                       else if (tstar  % 26 == 2)
-                       B = B2;
-                       else if (tstar  % 26 == 3)
-                       B = B3;
-                       else if (tstar  % 26 == 4)
-                       B = B4;
-                       else if (tstar  % 26 == 5)
-                       B = B5;
-                       else if (tstar  % 26 == 6)
-                       B = B6;
-                       else if (tstar  % 26 == 7)
-                       B = B7;
-                       else if (tstar  % 26 == 8)
-                       B = B8;
-                       else if (tstar  % 26 == 9)
-                       B = B9;
-                       else if (tstar  % 26 == 10)
-                       B = B10;
-                       else if (tstar  % 26 == 11)
-                       B = B11;
-                       else if (tstar  % 26 == 12)
-                       B = B12;
-                       else if (tstar  % 26 == 13)
-                       B = B13;
-                       else if (tstar  % 26 == 14)
-                       B = B14;
-                       else if (tstar  % 26 == 15)
-                       B = B15;
-                       else if (tstar  % 26 == 16)
-                       B = B16;
-                       else if (tstar  % 26 == 17)
-                       B = B17;
-                       else if (tstar  % 26 == 18)
-                       B = B18;
-                       else if (tstar  % 26 == 19)
-                       B = B19;
-                       else if (tstar  % 26 == 20)
-                       B = B20;
-                       else if (tstar  % 26 == 21)
-                       B = B21;
-                       else if (tstar  % 26 == 22)
-                       B = B22;
-                       else if (tstar  % 26 == 23)
-                       B = B23;
-                       else if (tstar  % 26 == 24)
-                       B = B24;
-                       else if (tstar  % 26 == 25)
-                       B = B25;
-                       else  
-                       B = B26;
-                       
-                       double sd = 628.4102;// 50% dynamical noise
-                       theta = rpois(m);
-                       noise = rnorm(0,sd);
-
-                       lambda = min(S,  B * (S) * pow(I + theta,alpha) );  // working great lambda was I
-
-                       
+                      #define max(x, y) (((x) > (y)) ? (x) : (y))
+                      #define min(x, y) (((x) < (y)) ? (x) : (y))
                       
-                       
+                      // By-week time  seasonality
+                      int  tstar = (t - 1944)*26.01786;
                       
-                       
-                       I = rnbinom_mu( I +1e-10, lambda);   // first arg should be 1/I   
-                        
-                       S = max( S + birth*(1-va) - I + noise ,0);
+                      
+                      if (tstar  % 26 == 1)
+                      B = B1;
+                      else if (tstar  % 26 == 2)
+                      B = B2;
+                      else if (tstar  % 26 == 3)
+                      B = B3;
+                      else if (tstar  % 26 == 4)
+                      B = B4;
+                      else if (tstar  % 26 == 5)
+                      B = B5;
+                      else if (tstar  % 26 == 6)
+                      B = B6;
+                      else if (tstar  % 26 == 7)
+                      B = B7;
+                      else if (tstar  % 26 == 8)
+                      B = B8;
+                      else if (tstar  % 26 == 9)
+                      B = B9;
+                      else if (tstar  % 26 == 10)
+                      B = B10;
+                      else if (tstar  % 26 == 11)
+                      B = B11;
+                      else if (tstar  % 26 == 12)
+                      B = B12;
+                      else if (tstar  % 26 == 13)
+                      B = B13;
+                      else if (tstar  % 26 == 14)
+                      B = B14;
+                      else if (tstar  % 26 == 15)
+                      B = B15;
+                      else if (tstar  % 26 == 16)
+                      B = B16;
+                      else if (tstar  % 26 == 17)
+                      B = B17;
+                      else if (tstar  % 26 == 18)
+                      B = B18;
+                      else if (tstar  % 26 == 19)
+                      B = B19;
+                      else if (tstar  % 26 == 20)
+                      B = B20;
+                      else if (tstar  % 26 == 21)
+                      B = B21;
+                      else if (tstar  % 26 == 22)
+                      B = B22;
+                      else if (tstar  % 26 == 23)
+                      B = B23;
+                      else if (tstar  % 26 == 24)
+                      B = B24;
+                      else if (tstar  % 26 == 25)
+                      B = B25;
+                      else  
+                      B = B26;
+                      
+                      
+                      theta = rpois(m);
+                      lambda = min( Sbar + Z,  B*(Sbar + Z)*pow(Tcases + theta,alpha));  // working great lambda was I
+                      
+                      
+                      I = rnbinom_mu( I, lambda);                   
+                      ")
 
 
-                       ")
-#' data should include birth
-#' S is a state 
-#' noise is a state
-#' parameter Sbar theta0
 
-dmeass <- Csnippet("
+#A overdispersed binomial mesurement is used here.
+
+dmeas <- Csnippet("
                   double psi = 0.116;
                   double mn = lambda/b1;
                   double v = mn*(1.0-1/b1+psi*psi*mn);
@@ -257,7 +235,7 @@ dmeass <- Csnippet("
                   }
                   ")
 
-rmeass <- Csnippet("
+rmeas <- Csnippet("
                   double psi = 0.116;
                   double mn = lambda/b1;
                   double v = mn*(1.0-1/b1+psi*psi*mn);
@@ -271,44 +249,52 @@ rmeass <- Csnippet("
                   ")
 
 
+
+##############################################################################################################################################
 pomp(London_cases,times="times",t0=1944,
-     rprocess=discrete.time.sim(step.fun=stochStepF,delta.t=1/26.01786),
+     rprocess=discrete.time.sim(step.fun=stochStep,delta.t=1/26.01786),
      paramnames=c("m","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11",
                   "B12","B13","B14","B15","B16","B17","B18","B19","B20","B21",
                   "B22","B23","B24","B25","B26",
                   "alpha","Sbar"),
-     initializer=initls,
+     initializer=initlz,
      params = c(m=13, B1 = 1.285935e-05, B2 = 1.214533e-05, B3 = 1.464063e-05, B4 = 1.277254e-05, B5 = 1.236623e-05, B6 = 1.181611e-05,
                 B7 = 1.059032e-05, B8 = 1.183479e-05, B9 = 1.024478e-05, B10 = 1.099727e-05,
                 B11 = 1.065480e-05, B12 = 1.136872e-05, B13 = 9.970371e-06, B14 = 9.655970e-06, B15 = 8.828046e-06, B16 = 8.999024e-06 ,
                 B17 = 6.775660e-06, B18 = 6.917613e-06, B19 = 7.245048e-06, B20 = 1.110509e-05,
                 B21 = 1.418629e-05, B22 = 1.017632e-05, B23 = 1.180947e-05, B24 = 1.052219e-05, B25 = 1.050035e-05 , B26 = 1.175871e-05,
                 alpha = 9.70e-01 , Sbar = 114281.6),
-     statenames=c("I","lambda","S","noise","theta"),
-     rmeasure = rmeass,
-     dmeasure = dmeass,
-     toEstimationScale = toEsts,
-     fromEstimationScale = fromEsts,
+     statenames=c("I","lambda","theta"),
+     rmeasure = rmeas,
+     dmeasure = dmeas,
+     toEstimationScale = toEst,
+     fromEstimationScale = fromEst,
      covar = Covar,
-     tcovar= "time") -> TSIRsim
+     tcovar= "time") -> TSIR
 
 
+plot(simulate(TSIR))
+firstFit <- mif2(TSIR, Nmif = 150, Np = 1000, #10 was 10000
+                 rw.sd = rw.sd(
+                   m=0.03, alpha=0.03, Sbar=0.03,
+                   B1 = .10, B2 = .10, B3 = .10, B4 = .10, B5 = .10, B6 = .10, B7 = .10, B8 = .10, B9 = .10, B10 = .10,
+                   B11 = .10, B12 = .10, B13 = .10, B14 = .10, B15 = .10, B16 = .10, B17 = .10, B18 = .10, B19 = .10, B20 = .10,
+                   B21 = .10, B22 = .10, B23 = .10, B24 = .10, B25 = .10, B26 = .10),
+                 transform = T,
+                 cooling.type = "geometric", cooling.fraction.50 = .05,
+                 tol = 1e-17, max.fail = Inf, verbose = getOption("verbose"))
 
+coef(TSIR)<-coef(firstFit)
 
-coef(TSIRsim) = coef(firstFit)
-
-
-
-#########################################################
-TSIRsim %>%
-  simulate(params=coef(TSIRsim),nsim=5,as.data.frame=TRUE,include.data=TRUE) %>%
+TSIR %>%
+  simulate(params=coef(firstFit),nsim=5,as.data.frame=TRUE,include.data=TRUE) %>%
   ggplot(aes(x=time,y=cases,group=sim,color=(sim=="data")))+
   guides(color=FALSE)+
-  geom_line()+facet_wrap(~sim,ncol=2) + ggtitle("TSIR model simulations with Data")+ theme_bw()
+  geom_line()+facet_wrap(~sim,ncol=2)+ ggtitle("TSIR Cases Reconstruction")+theme_bw()
 
 
-TSIRsim %>%
-  simulate(params=coef(firstFit),nsim=100,as.data.frame=TRUE,times= seq(1944,1985,by=7/365),include.data=TRUE) %>%
+TSIR %>%
+  simulate(params=coef(firstFit),nsim=100,as.data.frame=TRUE,include.data=TRUE) %>%
   subset(select=c(time,sim,cases)) %>%
   mutate(data=sim=="data") %>%
   ddply(~time+data,summarize,
@@ -317,26 +303,16 @@ TSIRsim %>%
          data=mapvalues(data,from=c(TRUE,FALSE),to=c("data","simulation"))) %>%
   dcast(time+data~p,value.var='q') %>%
   ggplot(aes(x=time,y=med,color=data,fill=data,ymin=lo,ymax=hi))+
-  geom_ribbon(alpha=0.2)+ theme_bw() + ylab("cases") + ggtitle("TSIR cases variability with Data")
-#
-####################################################'
-####################################################'
-"Mean Plotting"
-TSIRsim %>%
-  simulate(params=coef(firstFit),nsim=100,as.data.frame=TRUE,times= seq(1944,1985,by=7/365),include.data=TRUE) %>%
-  subset(select=c(time,sim,cases)) %>%
-  mutate(data=sim=="data") ->dta
-#
-  dta %>%
-  subset(sim!="data") %>% 
-  ddply( .(time), summarize, sim = "mean", cases=mean(cases), data = "mean") %>%
-  rbind(dta)->dta
-  
-ggplot(subset(dta,data !=FALSE), mapping=aes(x=time, y=cases, color=data)) + 
-    geom_line() + ggtitle("TSIR mean cases with data") + 
-    xlab("time") + ylab("cases") + theme_bw() 
+  geom_ribbon(alpha=0.2)+ theme_bw() + ylab("cases") + ggtitle("TSIR cases with Data")
 
 
+##########################################################################################
 
+# params = c(m=1, B1 = 2.695622e-06, B2 = 2.695622e-06, B3 = 2.695622e-06, B4 = 2.695622e-06, B5 = 2.695622e-06, B6 = 2.695622e-06,
+#            B7 = 2.695622e-06, B8 = 2.695622e-06, B9 = 2.695622e-06, B10 = 2.695622e-06,
+#            B11 = 2.695622e-06, B12 = 2.695622e-06, B13 = 2.695622e-06, B14 = 2.695622e-06, B15 = 2.695622e-06, B16 = 2.695622e-06,
+#            B17 = 2.695622e-06, B18 = 2.695622e-06, B19 = 2.695622e-06, B20 =2.695622e-06,
+#            B21 = 2.695622e-06, B22 = 2.695622e-06, B23 = 2.695622e-06, B24 = 2.695622e-06, B25 = 2.695622e-06, B26 = 2.695622e-06,
+#            alpha = 1 , Sbar = 481608)
 
 
